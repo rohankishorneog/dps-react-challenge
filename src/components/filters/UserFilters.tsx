4940f107376bb2ad54aebfb3c2f86c3a08f24975
rohankishorneog <rkn2601@gmail.com>
2024-12-13 00:30:43 +0530

    [userFilters] added props to UserFilters for handling onChange events

--- src/components/filters/UserFilters.tsx
+++ src/components/filters/UserFilters.tsx
@@ -1,10 +1,26 @@
 import Filter from './Filter';
 
-const UserFilters = () => {
+interface UserFiltersProps {
+	handleSearchCityValue: (city: string) => void;
+	handleSearchNameValue: (name: string) => void;
+}
+
+const UserFilters = ({
+	handleSearchCityValue,
+	handleSearchNameValue,
+}: UserFiltersProps) => {
 	return (
 		<div className="flex w-full justify-between items-center gap-4 border rounded-md p-2">
-			<Filter name="name" label="Name " handleChange={() => {}} />
-			<Filter name="city" label="City" handleChange={() => {}} />
+			<Filter
+				name="name"
+				label="Name "
+				handleChange={handleSearchNameValue}
+			/>
+			<Filter
+				name="city"
+				label="City"
+				handleChange={handleSearchCityValue}
+			/>
 			<Filter
 				name="oldest"
 				label="Highlight oldest in the city"
